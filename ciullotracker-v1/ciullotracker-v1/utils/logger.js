2dbd2354897c562702559635ee9a2df50df09f0a
DOrtenzio <dortenzio.diego.studente@itispaleocapa.it>
2026-09-11 09:53:06 +0200

    Revert "Add files via upload"
    This reverts commit ac7a6b1537639ecfc1fb62fb326fd04867232eb7.

--- ciullotracker-v1/ciullotracker-v1/utils/logger.js
+++
@@ -1,9 +0,0 @@
-const pino = require('pino');
-
-module.exports = pino({
-  level: process.env.LOG_LEVEL || 'info',
-  redact: {
-    paths: ['req.headers.cookie', 'req.headers.authorization', 'password', 'passwordHash', 'token', 'SESSION_SECRET'],
-    censor: '[REDACTED]'
-  }
-});
